0c435a542c45f2ef2ea01c91c7218fa4a7714a17
Jhonatanrs10 <jhonatanrs10@hotmail.com>
2023-10-19 22:37:27 -0300

    Att

--- dependencias.sh
+++ dependencias.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env sh
-npx expo install expo-clipboard @react-native-async-storage/async-storage @react-navigation/native react-native-screens react-native-safe-area-context @react-navigation/bottom-tabs react-native-fs expo-file-system 
+npx expo install expo-clipboard @react-native-async-storage/async-storage @react-navigation/native react-native-screens react-native-safe-area-context @react-navigation/bottom-tabs react-native-fs expo-file-system expo-system-ui react-native-web react-dom expo/webpack-config 
+npm remove expo-cli
